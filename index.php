961b70792b48363e25ea90074bda58ddcb5780bc
subway90 <nguyenhieu3105@gmail.com>
2025-04-04 00:01:04 +0700

    update index.php

--- index.php
+++ index.php
@@ -9,11 +9,7 @@ if(get_action_uri(0) == 'deploy-src') controller('user','deploy-src');
 if(LOCK_PAGE) controller('user','lock');
 
 # [ACTION]
-if (isset($_GET['act']) && $_GET['act']) {
-    $_arrayURL = explode('/', $_GET['act']);
-    $_action = $_arrayURL[0];
-    controller('user',$_action);
-}
+if ($action = get_action_uri(0)) controller('user',$action);
 
 # [DEFAULT]
 controller('user','kiem-tra');
